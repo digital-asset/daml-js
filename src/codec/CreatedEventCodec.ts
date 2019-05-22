718716b57ee0c52c6cf008b76c73c7b200c5d1ad
Stefano Baghino <stefano.baghino@digitalasset.com>
2019-05-22 15:36:51 +0200

    Add agreement text to created events
    Fixes #54

--- src/codec/CreatedEventCodec.ts
+++ src/codec/CreatedEventCodec.ts
@@ -1,32 +1,40 @@
 // Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-
 import {Codec} from "./Codec";
 import {CreatedEvent} from "../model/CreatedEvent";
 import {IdentifierCodec} from "./IdentifierCodec";
 import {RecordCodec} from "./RecordCodec";
-
 import {CreatedEvent as PbCreatedEvent} from "../generated/com/digitalasset/ledger/api/v1/event_pb";
+import {StringValue} from "google-protobuf/google/protobuf/wrappers_pb";
 
 export const CreatedEventCodec: Codec<PbCreatedEvent, CreatedEvent> = {
-    deserialize(event: PbCreatedEvent): CreatedEvent {
-        return {
+    deserialize(message: PbCreatedEvent): CreatedEvent {
+        const object: CreatedEvent = {
             eventType: "created",
-            eventId: event.getEventId(),
-            contractId: event.getContractId(),
-            templateId: IdentifierCodec.deserialize(event.getTemplateId()!),
-            arguments: RecordCodec.deserialize(event.getCreateArguments()!),
-            witnessParties: event.getWitnessPartiesList()
+            eventId: message.getEventId(),
+            contractId: message.getContractId(),
+            templateId: IdentifierCodec.deserialize(message.getTemplateId()!),
+            arguments: RecordCodec.deserialize(message.getCreateArguments()!),
+            witnessParties: message.getWitnessPartiesList()
         };
+        if (message.hasAgreementText()) {
+            object.agreementText = message.getAgreementText()!.getValue();
+        }
+        return object;
     },
-    serialize(event: CreatedEvent): PbCreatedEvent {
-        const result = new PbCreatedEvent();
-        result.setEventId(event.eventId);
-        result.setContractId(event.contractId);
-        result.setTemplateId(IdentifierCodec.serialize(event.templateId));
-        result.setCreateArguments(RecordCodec.serialize(event.arguments));
-        result.setWitnessPartiesList(event.witnessParties);
-        return result;
+    serialize(object: CreatedEvent): PbCreatedEvent {
+        const message = new PbCreatedEvent();
+        message.setEventId(object.eventId);
+        message.setContractId(object.contractId);
+        message.setTemplateId(IdentifierCodec.serialize(object.templateId));
+        message.setCreateArguments(RecordCodec.serialize(object.arguments));
+        message.setWitnessPartiesList(object.witnessParties);
+        if (object.agreementText !== undefined) {
+            const agreementText = new StringValue();
+            agreementText.setValue(object.agreementText);
+            message.setAgreementText(agreementText);
+        }
+        return message;
     }
 };
