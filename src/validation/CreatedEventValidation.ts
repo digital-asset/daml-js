97ad322cbb3db0cd661c304415fa5659eda90b61
Stefano Baghino <43749967+stefanobaghino-da@users.noreply.github.com>
2019-05-22 16:22:30 +0200

    Add agreement text to created events (#55)
    Fixes #54

--- src/validation/CreatedEventValidation.ts
+++ src/validation/CreatedEventValidation.ts
@@ -1,14 +1,13 @@
 // Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-
 import {native} from "./Native";
 import {CreatedEvent} from "../model/CreatedEvent";
 import {RecordValidation} from "./RecordValidation";
 import {IdentifierValidation} from "./IdentifierValidation";
 import {array} from "./Array";
 import {object} from "./Object";
-import {noFields, RequiredFieldsValidators} from "./Validation";
+import {OptionalFieldsValidators, RequiredFieldsValidators} from "./Validation";
 import {string} from "./String";
 
 function required(): RequiredFieldsValidators<CreatedEvent> {
@@ -22,4 +21,10 @@ function required(): RequiredFieldsValidators<CreatedEvent> {
     };
 }
 
-export const CreatedEventValidation = object<CreatedEvent>('CreatedEvent', required, noFields);
+function optional(): OptionalFieldsValidators<CreatedEvent> {
+    return {
+        agreementText: native('string')
+    };
+}
+
+export const CreatedEventValidation = object<CreatedEvent>('CreatedEvent', required, optional);
