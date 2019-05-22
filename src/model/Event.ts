a62619a09f09366396eb511a85df813b5d285927
Stefano Baghino <stefano.baghino@digitalasset.com>
2019-05-22 15:08:13 +0200

    Remove ExercisedEvent from Event union
    Fixes #52

--- src/model/Event.ts
+++ src/model/Event.ts
@@ -1,8 +1,7 @@
 // Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import {ExercisedEvent} from "./ExercisedEvent";
 import {CreatedEvent} from "./CreatedEvent";
 import {ArchivedEvent} from "./ArchivedEvent";
 
-export type Event = ArchivedEvent | CreatedEvent | ExercisedEvent;
+export type Event = ArchivedEvent | CreatedEvent;
