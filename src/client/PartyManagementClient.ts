a546615817ad61a8af8ed8f64c00321ee866a00d
Paul Sitoh <paulwizviz@gmail.com>
2019-08-08 11:39:43 +0100

    Added more implementation to retrieve parties details
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- src/client/PartyManagementClient.ts
+++ src/client/PartyManagementClient.ts
@@ -1,6 +1,11 @@
 // Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+import {ClientCancellableCall} from "../call/ClientCancellableCall";
+import {Callback} from "../util/Callback";
+import {ClientReadableObjectStream} from "../call/ClientReadableObjectStream";
+import {ListKnownPartiesResponse} from "../model/ListKnowPartiesResponse";
+
 export interface PartyManagementClient{
-    
+    getParties(): ClientReadableObjectStream<ListKnownPartiesResponse>
 }
