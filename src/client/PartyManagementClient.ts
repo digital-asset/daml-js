7b24cd53f350bcda0bdd00e8aa8e3010261d24bc
Paul Sitoh <paulwizviz@gmail.com>
2019-08-08 14:28:25 +0100

    Update GRPC implementation
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- src/client/PartyManagementClient.ts
+++ src/client/PartyManagementClient.ts
@@ -1,11 +1,10 @@
 // Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import {ClientCancellableCall} from "../call/ClientCancellableCall";
-import {Callback} from "../util/Callback";
+import {ListKnownPartiesRequest} from "../model/ListKnownPartiesRequest";
 import {ClientReadableObjectStream} from "../call/ClientReadableObjectStream";
-import {ListKnownPartiesResponse} from "../model/ListKnowPartiesResponse";
+import {ListKnownPartiesResponse} from "../model/ListKnownPartiesResponse";
 
 export interface PartyManagementClient{
-    getParties(): ClientReadableObjectStream<ListKnownPartiesResponse>
+    listKnownParties(requestObject: ListKnownPartiesRequest): ClientReadableObjectStream<ListKnownPartiesResponse>
 }
