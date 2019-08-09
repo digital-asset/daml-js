a531a93a439351e1d8a8b8ca866751d6a033139d
Paul Sitoh <paulwizviz@gmail.com>
2019-08-09 15:25:13 +0100

    Update ListKnownPartiesRequestValidation
    Check to see that there is no field.

--- src/validation/ListKnownPartiesRequestValidation.ts
+++ src/validation/ListKnownPartiesRequestValidation.ts
@@ -1,22 +1,8 @@
 // Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import {TransactionFilterValidation} from "./TransactionFilterValidation";
 import {ListKnownPartiesRequest} from "../model/ListKnownPartiesRequest";
-import {OptionalFieldsValidators, RequiredFieldsValidators} from "./Validation";
-import {native} from "./Native";
+import {noFields} from "./Validation";
 import {object} from "./Object";
 
-function required(): RequiredFieldsValidators<ListKnownPartiesRequest> {
-    return {
-        filter: TransactionFilterValidation,
-    };
-}
-
-function optional(): OptionalFieldsValidators<ListKnownPartiesRequest> {
-    return {
-        verbose: native('boolean'),
-    }
-}
-
-export const ListKnownPartiesRequestValidation = object<ListKnownPartiesRequest>('GetActiveContractsRequest', required, optional);
+export const ListKnownPartiesRequestValidation = object<ListKnownPartiesRequest>('ListKnownPartiesRequest', noFields, noFields);
