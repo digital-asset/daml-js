e1087d5723a49c8b62f135e57efa9b122c439a5e
Paul Sitoh <paulwizviz@gmail.com>
2019-08-09 12:53:05 +0100

    Update party detail type
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- src/model/PartyDetail.ts
+++ src/model/PartyDetail.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-export interface PartyDetails {
+export interface PartyDetail {
 
     // The stable unique identifier of a DAML party.
     // Must be a valid PartyIdString (as described in ``value.proto``).
