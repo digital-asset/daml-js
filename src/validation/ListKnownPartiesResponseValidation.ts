598ac2dd3844bdb2ae68288df30bed31d5eee910
Paul Sitoh <paulwizviz@gmail.com>
2019-08-09 16:34:03 +0100

    Fix ListKnownPartiesResponseValidation
    Add the necessary code to deal with parties validation.

--- src/validation/ListKnownPartiesResponseValidation.ts
+++ src/validation/ListKnownPartiesResponseValidation.ts
@@ -1,24 +1,16 @@
 // Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import {CreatedEventValidation} from "./CreatedEventValidation";
 import {ListKnownPartiesResponse} from "../model/ListKnownPartiesResponse";
-import {OptionalFieldsValidators, RequiredFieldsValidators} from "./Validation";
+import {PartyDetailsValidation} from "./PartyDetailsValidation";
+import {noFields, RequiredFieldsValidators} from "./Validation";
 import {array} from "./Array";
-import {native} from "./Native";
 import {object} from "./Object";
 
 function required(): RequiredFieldsValidators<ListKnownPartiesResponse> {
     return {
-        parties: array()
+        partyDetails: array(PartyDetailsValidation),
     };
 }
 
-function optional(): OptionalFieldsValidators<ListKnownPartiesResponse> {
-    return {
-        activeContracts: array(CreatedEventValidation),
-        workflowId: native('string')
-    }
-}
-
-export const ListKnownPartiesResponseValidation = object<ListKnownPartiesResponse>('ListKnownPartiesResponse', required, optional);
+export const ListKnownPartiesResponseValidation = object<ListKnownPartiesResponse>('ListKnownPartiesResponse', required, noFields);
