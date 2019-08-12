6cbff5800a610fe40fc01b6b83b383bf2e397ead
Paul Sitoh <paulwizviz@gmail.com>
2019-08-12 12:10:51 +0100

    Fix party details codec
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- src/codec/PartyDetailsCodec.ts
+++ src/codec/PartyDetailsCodec.ts
@@ -7,19 +7,25 @@ import {PartyDetails as PbPartyDetails} from "../generated/com/digitalasset/ledg
 
 export const PartyDetailsCodec: Codec<PbPartyDetails, PartyDetails> = {
     deserialize(response: PbPartyDetails): PartyDetails {
-        return {
-            party: response.getParty(),
-            displayName: response.getDisplayName(),
-            isLocal: response.getIsLocal()
-        };
+        const displayName = response.getDisplayName();
+        if (response.getDisplayName() !== undefined) {
+            return {
+                party: response.getParty(),
+                displayName: displayName,
+                isLocal: response.getIsLocal()
+            };
+        } else {
+            return {
+                party: response.getParty(),
+                displayName: "",
+                isLocal: response.getIsLocal()
+            }
+        }
     },
     serialize(response: PartyDetails): PbPartyDetails {
         const result = new PbPartyDetails();
         result.setParty(response.party);
-        if (response.displayName !== undefined) {
-            const dName:string = response.displayName;
-            result.setDisplayName(dName);
-        }
+        result.setDisplayName(<string> response.displayName);
         result.setIsLocal(response.isLocal);
         return result;
     }
