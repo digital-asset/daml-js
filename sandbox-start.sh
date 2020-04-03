839d769ebb84958640e12aefaa0bc259abe2cf05
Stefano Baghino <stefano.baghino@digitalasset.com>
2020-04-03 13:23:27 +0200

    SDK assistant is not in the PATH on CI

--- sandbox-start.sh
+++ sandbox-start.sh
@@ -7,8 +7,8 @@ set -euxo pipefail
 cd "$(dirname "${0}")"
 DAML_SRC=test/integration/src
 
-(cd "$DAML_SRC" && "$HOME"/.daml/bin/daml build && daml damlc inspect-dar .daml/dist/IntegrationTests-0.0.0.dar | tail -n 1 | awk '{print $2}' | tr -d '"' > ../IntegrationTests-0.0.0.sha256)
-(cd "$DAML_SRC/uploadDar" && "$HOME"/.daml/bin/daml build && daml damlc inspect-dar .daml/dist/UploadDarIntegrationTests-0.0.0.dar | tail -n 1 | awk '{print $2}' | tr -d '"' > ../../UploadDarIntegrationTests-0.0.0.sha256)
+(cd "$DAML_SRC" && "$HOME"/.daml/bin/daml build && "$HOME"/.daml/bin/daml damlc inspect-dar .daml/dist/IntegrationTests-0.0.0.dar | tail -n 1 | awk '{print $2}' | tr -d '"' > ../IntegrationTests-0.0.0.sha256)
+(cd "$DAML_SRC/uploadDar" && "$HOME"/.daml/bin/daml build && "$HOME"/.daml/bin/daml damlc inspect-dar .daml/dist/UploadDarIntegrationTests-0.0.0.dar | tail -n 1 | awk '{print $2}' | tr -d '"' > ../../UploadDarIntegrationTests-0.0.0.sha256)
 
 PORT=$(node reserve-port)
 echo "$PORT" > sandbox.port
