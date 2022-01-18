e136fb53e6660997068a8a7632db23cc3dd98d43
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2022-01-18 18:38:15 +0100

    Bump copyright year (#136)

--- src/client/NodeJsLedgerConfigurationClient.ts
+++ src/client/NodeJsLedgerConfigurationClient.ts
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 import {GetLedgerConfigurationRequest} from "../generated/com/daml/ledger/api/v1/ledger_configuration_service_pb";
